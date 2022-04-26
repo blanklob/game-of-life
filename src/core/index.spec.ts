a18afd4e3b45ebfda05f85d8ee3e31f08e6fdf7e
Youness Id bakkasse <std.youness@gmail.com>
2022-04-26 02:26:06 +0200

    Fixed issue

--- src/core/index.spec.ts
+++ src/core/index.spec.ts
@@ -5,8 +5,9 @@ import { createLife } from './index';
 
 describe('Test the creation of the first cells', function () {
   it('Should pass', function () {
+    const dimensions: [number, number] = [1000, 1000];
     // Check if rendom cell coords are outside the grid.
-    const livingCellsGeneration = createLife(100);
+    const livingCellsGeneration = createLife(dimensions, 100);
 
     expect(livingCellsGeneration.length).toEqual(100);
     // Create a loop that verifies the randomly generated cells coordinants
